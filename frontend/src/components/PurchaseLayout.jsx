57e5843531768c7b30a099b06fa8bcbb59036365
jsan956-ai <jsan956@aucklanduni.ac.nz>
2023-03-30 12:18:46 +1300

    react context classes are set up and linked with the Cart component successfully

--- frontend/src/components/PurchaseLayout.jsx
+++ frontend/src/components/PurchaseLayout.jsx
@@ -1,10 +1,28 @@
-import React from 'react'; 
+import React, { useContext } from 'react';
+import CartContext from '../store/cart-context';
 
 const PurchaseLayout = () => { 
+    const cartContext = useContext(CartContext);
+
+    const incrementCartTotalAmountHandler = () => {
+        cartContext.incrementTotalAmount();
+    };   
+
+    const decrementCartTotalAmountHandler = () => {
+        cartContext.decrementTotalAmount();
+    };
+
+
     return (
-        <div> 
-           <h1>This is the purchase layout</h1>
-        </div>
+        <>
+            <div> 
+                <h1>This is the purchase layout</h1>
+            </div>
+            <div>
+                <button onClick={incrementCartTotalAmountHandler}>+</button>
+                <button onClick={decrementCartTotalAmountHandler}>-</button>
+            </div>
+        </>
     );
 }
 
