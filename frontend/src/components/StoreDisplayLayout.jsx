2dc7b1e831bc4166d39358ee5dd881ce26d36ef3
Janet <2293465572@qq.com>
2023-04-11 18:31:37 +1200

    card_styling

--- frontend/src/components/StoreDisplayLayout.jsx
+++ frontend/src/components/StoreDisplayLayout.jsx
@@ -1,23 +1,30 @@
 import React from 'react'; 
 import { Link } from 'react-router-dom';
 import Card from '../pages/ui/Card';
-
+import { StarFill } from 'react-bootstrap-icons';
 const StoreDisplayLayout = (props) => { 
-    
     console.log(props.items)
-
-
     return (
-        <div> 
-           <h1>All items in store or filted by category</h1>
-           
-           <ul>
+        <div > 
+            <h1>All items in store or filted by category</h1>
+            <ul className="row list-unstyled" >
                 {props.items.map((item) => (
-                    <li key={item.pid}>
+                    <li key={item.pid} className="col-sm-4">
                         <Card>
                             <div >
-                                <Link id="productLink" to={`/store/product/${item.pid}`}><h2>{item.name}</h2></Link>
-                                <p>author: <Link id="authorLink" to={`/store/author/${item.aid}`}>{item.author}</Link></p>
+                                <div>
+                                    <img src={item.url}/>
+                                </div>
+                                <div>
+                                    <Link id="productLink" to={`/store/product/${item.pid}`}><h2>{item.name}</h2></Link>
+                                    <h6><h3>${Math.floor(item.price)}</h3>{(item.price % 1).toFixed(2).split('.')[1]}</h6>
+                                </div>
+                                {/* <p>author: <Link id="authorLink" to={`/store/author/${item.aid}`}>{item.author}</Link></p> */}
+                                <p>{item.intro}</p>
+                                <div className="d-flex justify-content-between">
+                                    <h5>{item.sold} Sold</h5>
+                                    <h5 className="float-right"> <StarFill color="black" size={18} />&nbsp;{item.like.toFixed(1)}</h5>
+                                </div>
                             </div>
                         </Card> 
                     </li>
@@ -26,5 +33,4 @@ const StoreDisplayLayout = (props) => {
         </div>
     );
 }
-
 export default StoreDisplayLayout;
--- frontend/src/pages/ProductSearchPage.jsx
+++ frontend/src/pages/ProductSearchPage.jsx
@@ -8,24 +8,36 @@ const DUMMY_DATA = [
         pid: 1,
         aid: 2,
         name: 'goku',
+        price:80.00,
+        sold:414,
+        like:4.8,
         url: 'https://www.cartonionline.com/wordpress/wp-content/uploads/2023/02/goku-814x1024.jpg',
         author: 'steve',
+        intro:'This is a brief introduction to the product.',
         description: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     },
     {   
         pid: 3,
         aid: 4,
         name: 'naruto',
+        price:5.50,
+        sold:8545,
+        like:3.2,
         url: 'https://animecorner.me/wp-content/uploads/2022/10/naruto.png',
         author: 'herobrine',
+        intro:'This is a brief introduction to the product.',
         description: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     },
     {   
         pid: 5,
         aid: 6,
         name: 'sasuke',
+        price:11.99,
+        sold:23,
+        like:5.0,
         url: 'https://www.looper.com/img/gallery/every-power-sasuke-has-on-naruto-explained/intro-1663193400.jpg',
         author: 'bob',
+        intro:'This is a brief introduction to the product.',
         description: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     },
     
