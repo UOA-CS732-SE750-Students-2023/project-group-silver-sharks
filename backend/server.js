e17413f7aee6c66fb4d3c0924519e99ba5a54e38
Nilay Setiya <79784352+nset793@users.noreply.github.com>
2023-04-12 17:36:23 +1200

    Bug fix with router

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUI from "swagger-ui-express";
 import connectDB from "./db/mongoose.js";
+import router from "./routes/example.js";
 
 const app = express();
 
@@ -27,9 +28,7 @@ const swaggerSpec = swaggerJSDoc(swaggerOptions);
 // Serve API documentation
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
-app.get("/hello", (req, res) => {
-  res.send("Hello World");
-});
+app.use(router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
