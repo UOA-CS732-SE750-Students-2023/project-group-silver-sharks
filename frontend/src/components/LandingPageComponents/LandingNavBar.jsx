563811723e7161899efe8c2f194b4247e26db2d3
Bryan LIu <bliu631@aucklanduni.ac.nz>
2023-04-13 12:10:09 +1200

    change pathing from sign up to login

--- frontend/src/components/LandingPageComponents/LandingNavBar.jsx
+++ frontend/src/components/LandingPageComponents/LandingNavBar.jsx
@@ -9,7 +9,7 @@ function LandingNavBar() {
         <div className="landing-page-navbar-wrapper">
           <div className="sign-in-row">
             <div className="landing-page-logo"><b>SHARKET</b>PLACE</div>
-            <div className="landing-page-sign-in"><a href='/signup'><button className="sign-in-button">Sign In</button></a></div>
+            <div className="landing-page-sign-in"><a href='/login'><button className="sign-in-button">Sign In</button></a></div>
           </div>
           <hr className='landing-page-navbar-line'/>
           <div className='landing-page-desc'>
