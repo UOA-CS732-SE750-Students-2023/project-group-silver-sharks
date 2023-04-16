e8544745a75dbded62ac9f09629ee5c43c35a4f1
IbrahimAnees <79844171+iane056@users.noreply.github.com>
2023-04-16 13:59:42 +1200

    add account

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,16 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
-import { getAllAccounts } from "../dao/account-dao.js";
+import { 
+    getAllAccounts,
+    addAccount
+} from "../dao/account-dao.js";
 
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
 const accountRouter = new express.Router();
 
 // endpoint 1: GET all accounts without worrying about pagination
-accountRouter.get("/accounts", async (req, res) => {
+accountRouter.get("/account", async (req, res) => {
   const { accounts, count } = await getAllAccounts();
 
   return res.status(StatusCodes.OK).header("Count", count).json(accounts);
 });
 
 export default accountRouter;
+
+// endpoint 2: POST - adding account
+accountRouter.post('/account', async (req,res) => {
+    const newAccount = await addAccount(req.body);
+
+    return res.status(StatusCodes.CREATED)
+              .header('Location', `/products/${newAccount._id}`)
+              .json(newAccount);
+});
