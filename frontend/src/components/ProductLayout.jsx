fb2799388fa389a3714faf044f934b6d141419dd
jsan956-ai <jsan956@aucklanduni.ac.nz>
2023-04-23 22:47:04 +1200

    product search bar functionality working and loaders up for product detail page

--- frontend/src/components/ProductLayout.jsx
+++ frontend/src/components/ProductLayout.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 const ProductLayout = ({ product }) => { 
 
-    // errorpage needs its own separate nav bar
     return (
         <div>
             <h1>{product.name}</h1>
-            <img src={product.url} alt={product.name} />
             <p>{product.description}</p>
+            <p>{`Price: ${product.price}`}</p>
         </div>
     );
 }
