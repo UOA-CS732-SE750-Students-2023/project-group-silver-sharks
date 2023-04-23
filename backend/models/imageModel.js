ec6eaef5ee9ebb26a2d1983a78c3deb087a3c0e3
Nilay Setiya <79784352+nset793@users.noreply.github.com>
2023-04-23 13:49:51 +1200

    Added basic image endpoints

--- backend/models/imageModel.js
+++ backend/models/imageModel.js
@@ -4,10 +4,6 @@ const Schema = mongoose.Schema;
 
 const imageSchema = new Schema(
   {
-    _id: {
-      type: String,
-      required: true,
-    },
     imgName: {
       type: String,
       required: true,
