562340694d44333b81c141f59661d8eb6d91e05f
Janet <2293465572@qq.com>
2023-04-25 03:46:15 +1200

    account page and cart, update card

--- frontend/src/pages/AuthorPage.jsx
+++ frontend/src/pages/AuthorPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import AuthorLayout from '../components/AuthorLayout';
+import AuthorLayoutUserview from '../components/AuthorLayoutUserview';
 
 
 const DUMMY_DATA = [
@@ -8,18 +9,30 @@ const DUMMY_DATA = [
         aid: 2,
         url: 'https://upload.wikimedia.org/wikipedia/en/e/e7/Steve_%28Minecraft%29.png',
         name: 'steve',
+        average_rating:4.4,
+        average_response:'2 days',
+        assets_sold:985,
+        account_type:'Member',
         bio: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     },
     {   
         aid: 4,
         url: 'https://attackofthefanboy.com/wp-content/uploads/2023/02/Herobrine-Minecraft.jpg',
         name: 'herobrine',
+        average_rating:4.4,
+        average_response:'2 days',
+        assets_sold:985,
+        account_type:'Member',
         bio: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     },
     {   
         aid: 6,
         url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFZUlClbYfJUR1_KPHgV9OGJcxK-LSzDruDQ&usqp=CAU',
         name: 'bob',
+        average_rating:4.4,
+        average_response:'2 days',
+        assets_sold:985,
+        account_type:'Member',
         bio: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
     },
     
@@ -28,6 +41,8 @@ const DUMMY_DATA = [
 
 const AuthorPage = () => { 
     const [authors,setAuthors] = useState(DUMMY_DATA);
+    
+    const [role,setRole] = useState('user');
 
     const params = useParams();
 
@@ -39,9 +54,23 @@ const AuthorPage = () => {
         }
     })
 
-    console.log(authorDetails)
+    console.log(authorDetails) 
+    
+    const whovisit=()=>{
+        if(current_aid===authorpage_aid){
+            setRole('admin')
+        }
+        else{
+            setRole('user')
+        }
+
+    }
+    
     return (
-        <AuthorLayout author={authorDetails}/>
+        <div>
+            {role === 'admin' && <AuthorLayout author={authorDetails}/>}
+            {role === 'user' && <AuthorLayoutUserview author={authorDetails}/>}
+        </div>
     );
 }
 
