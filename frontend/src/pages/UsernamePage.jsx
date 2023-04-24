b49fe2f7474847ac53e66c1d7836c7507b649434
Bryan LIu <bliu631@aucklanduni.ac.nz>
2023-04-24 21:29:01 +1200

    Fix navbar for store page

---
+++ frontend/src/pages/UsernamePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import LandingNavBar from '../components/LandingPageComponents/LandingNavBar';
+import UsernameLayout from '../components/UsernamePageComponents/UsernameLayout';
+
+const UsernamePage = () => { 
+
+    return (
+        <>
+            <LandingNavBar />
+            <UsernameLayout />
+        </>
+    );
+}
+
+export default UsernamePage;
