8ebaa1b10d18ad65622d869502c67649917693cc
jsan956-ai <jsan956@aucklanduni.ac.nz>
2023-04-25 16:41:29 +1200

    authentication connected and running in the production env, getting the username in the nav bar

--- frontend/src/components/StoreComponents/StoreNavBar.jsx
+++ frontend/src/components/StoreComponents/StoreNavBar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'; 
-import { NavLink, useNavigate } from 'react-router-dom';
+import { useNavigate,useRouteLoaderData,json } from 'react-router-dom';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import MenuIcon from '@mui/icons-material/Menu';
 import './StoreNavBar.css';
 
 const StoreNavBar = (props) => { 
+
+    const user = useRouteLoaderData('username-loader');
+
+    console.log("from inside the component")
+    console.log(user);
+
     const navigate = useNavigate();
 
     const profileNavigationHandler = () => { 
@@ -23,7 +29,7 @@ const StoreNavBar = (props) => {
         <header className="store-navbar-container">
             <div className="store-navbar-wrapper">
                 <div className="store-navbar-user-info">
-                    <p>Logged in as <b>XXX</b></p>
+                    <p>Logged in as <b>{user.username}</b></p>
                 </div>
                 <div className="store-navbar-action-row">
                     <div className="brand-name"><b>SHARKET</b>PLACE</div>
@@ -39,3 +45,14 @@ const StoreNavBar = (props) => {
 }
 
 export default StoreNavBar;
+
+export const loader = async () => {
+
+    const response = await fetch('http://localhost:3000/account/id/0');
+
+    if (!response.ok) {
+        return json({ message: "Could not fetch events."}, { status: 500 });
+    } else {
+        return response;
+    }
+}
