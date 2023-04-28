95e581b9b03d8573450fa35d640809c33e7d7d19
bliu631 <79785035+bliu631@users.noreply.github.com>
2023-04-28 22:51:18 +1200

    Revert "added 3 product review endpoints"

--- backend/models/productReviewModel.js
+++ backend/models/productReviewModel.js
@@ -2,32 +2,28 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const productReviewSchema = new Schema(
-  {
-    text: {
-      type: String,
-      required: true,
-    },
-    rating: {
-      type: Number,
-      required: true,
-      min: 1,
-      max: 5,
-    },
-    product: {
-      type: Schema.Types.ObjectId,
-      ref: "Product",
-    },
-    account: {
-      type: String,
-      ref: "Account",
-    },
+const productReviewSchema = new Schema({
+  text: {
+    type: String,
+    required: true
   },
-  {
-    timestamps: {},
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: "Product"
+  },
+  account: {
+    type: Schema.Types.ObjectId,
+    ref: "Account"
   }
-);
-
+},{
+  timestamps: {}
+});
 
 export const ProductReview = mongoose.model(
   "ProductReview",
