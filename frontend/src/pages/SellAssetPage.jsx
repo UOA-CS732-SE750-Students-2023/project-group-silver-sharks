199d46b9b223a1f4aa7a55acdd82f5f0170e0866
jsan956-ai <jsan956@aucklanduni.ac.nz>
2023-04-30 05:32:00 +1200

    created partial action for the sell-assets page

--- frontend/src/pages/SellAssetPage.jsx
+++ frontend/src/pages/SellAssetPage.jsx
@@ -9,3 +9,40 @@ const SellAssetPage = () => {
 }
 
 export default SellAssetPage;
+
+export const action = async ({request,params}) => {
+    
+    // get the user id 
+    // /account/id/:id
+
+    // getting the form data from request argument 
+    const formData = await request.formData();
+    const sellingProductData = {
+        title: formData.get('title'),
+        description: formData.get('description'),
+        category: formData.get("category"),
+        price: +formData.get('price')
+    };
+  
+    // getting the http method from the request argument
+    //const method = request.method;
+
+    console.log("**************************************************");
+    console.log(sellingProductData);
+    console.log("**************************************************");
+
+    /*
+    // NEED THE USER ID HERE
+    
+    // getting the form data from request argument 
+    const formData = await request.formData();
+    const sellingProductData = {
+        title: formData.get('title'),
+        image: formData.get('image'),
+        date: formData.get('date'),
+        description: formData.get('description'),
+    };
+    */
+   
+    return true;
+};
