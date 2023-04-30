fd471fc45b9e65268f5e5062e5a221e4b5d86e9a
jsan956-ai <jsan956@aucklanduni.ac.nz>
2023-05-01 06:37:32 +1200

    single product image upload working

--- frontend/src/components/SellAssetLayout.jsx
+++ frontend/src/components/SellAssetLayout.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate, useActionData, Form, json } from "react-router-dom";
+import { useNavigate, useActionData, Form, json, redirect } from "react-router-dom";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import "./SellAssetLayout.css";
 
@@ -8,7 +8,7 @@ const SellAssetLayout = ({ userId }) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredDescription, setEnteredDescription] = useState('');
   const [coverImage, setCoverImage] = useState('');
-  const [files, setFiles] = useState('');
+  const [files, setFiles] = useState([]);
   const [category, setCategory] = useState('Images'); 
   const [price, setPrice] = useState(0);
 
@@ -16,11 +16,6 @@ const SellAssetLayout = ({ userId }) => {
   const data = useActionData();
 
   const navigate = useNavigate();
-
-  // return to previous page
-  function cancelHandler() {
-    navigate("..");
-  }
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -36,24 +31,30 @@ const SellAssetLayout = ({ userId }) => {
   };
 
   const filesChangeHandler = (event) => {
-    //const enteredFiles = Array.prototype.slice.call(event.target.files)
-    setFiles(event.target.files);
+    const files = event.target.files;
+    const newFiles = [];
+  
+    for (let i = 0; i < files.length; i++) {
+      newFiles.push(files[i]);
+    }
+  
+    setFiles(newFiles);
   };
+  
 
   const priceChangeHandler = (event) => {
     setPrice(event.target.value);
   };
 
   const categoryChangeHandler = (event) => {
-    console.log(event.target.value, 48)
     setCategory(event.target.value);
   };
 
   const submitHandler = async (event) => {
     event.preventDefault();
-
-    // print all the data returned from the form 
-    console.log("after form submission !!!"); 
+  
+    // print all the data returned from the form
+    console.log("after form submission !!!");
     console.log(enteredTitle, 56);
     console.log(enteredDescription, 57);
     console.log(price, 58);
@@ -61,57 +62,100 @@ const SellAssetLayout = ({ userId }) => {
     console.log(coverImage, 60);
     console.log(files, 61);
     console.log("END");
-
+  
     // print the user id that will be needed to create the product in the backend
     console.log(userId, 65);
-
+  
     const productData = {
-      name: enteredTitle, 
-      description: enteredDescription, 
-      category: category, 
-      price: price
-    }
-
+      name: enteredTitle,
+      description: enteredDescription,
+      category: category,
+      price: price,
+    };
+  
     // print the user id that will be needed to create the product in the backend
     console.log(userId, 65);
-
-    const textResponse = await fetch('http://localhost:3000/products/sell/' + userId, {
+  
+    const textResponse = await fetch(
+      "http://localhost:3000/products/sell/" + userId,
+      {
         method: "POST",
         headers: {
-            'Content-Type': 'application/json',   // */*
+          "Content-Type": "application/json", // */*
         },
-        body: JSON.stringify(productData)
-    });
+        body: JSON.stringify(productData),
+      }
+    );
   
-    if (!textResponse.ok){
-        throw json({ message: "Could not successfully submit the text data for the sell assets action."}, { status: 500 });
+    if (!textResponse.ok) {
+      throw json(
+        {
+          message:
+            "Could not successfully submit the text data for the sell assets action.",
+        },
+        { status: 500 }
+      );
     }
-
-    const newProduct = await textResponse.json()
-
-    console.log("***************************************************")
+  
+    const newProduct = await textResponse.json();
+  
+    console.log("***************************************************");
     console.log(newProduct._id);
     console.log(newProduct.name);
-    console.log("***************************************************")
-
-    // 
-
+    console.log("***************************************************");
+  
     // second post request to submit the cover image
     const coverImageFormData = new FormData();
     coverImageFormData.append("files", coverImage);
-
-    const fileResponse = await fetch('http://localhost:3000/upload-coverimage/' + newProduct._id, {
+  
+    const coverImageResponse = await fetch(
+      "http://localhost:3000/upload-coverimage/" + newProduct._id,
+      {
         method: "POST",
-        body: coverImageFormData
-    });
-
-    if (!fileResponse.ok){
-        throw json({ message: "Could not successfully submit the cover image for the sell assets action."}, { status: 500 });
+        body: coverImageFormData,
+      }
+    );
+  
+    if (!coverImageResponse.ok) {
+      throw json(
+        {
+          message:
+            "Could not successfully submit the cover image for the sell assets action.",
+        },
+        { status: 500 }
+      );
     }
-    
-    console.log("done")
-
+  
+    // third post request to upload the actual art files
+    const productFilesFormData = new FormData();
+  
+    for (let i = 0; i < files.length; i++) {
+      productFilesFormData.append("files", files[i]);
+    }
+  
+    console.log(productFilesFormData, 112);
+  
+    const fileResponse = await fetch(
+      "http://localhost:3000/upload-downloadfiles/" + newProduct._id,
+      {
+        method: "POST",
+        body: productFilesFormData,
+      }
+    );
+  
+    if (!fileResponse.ok) {
+      throw json(
+        {
+          message:
+            "Could not successfully submit the product files for the sell assets action.",
+        },
+        { status: 500 }
+      );
+    }
+  
+    redirect("/store/profile/selling");
   };
+  
 
   return (
     <Container fluid className="container-fluid">
@@ -222,54 +266,3 @@ const SellAssetLayout = ({ userId }) => {
 };
 
 export default SellAssetLayout;
-
-/*
-  <RouterForm method="POST" action="/store/sell-asset" >
-            <Form className="form-container">
-              <FormGroup>
-                <FormControl id="title" type="text" name="title" placeholder="Title" required />
-              </FormGroup>
-  
-              <FormGroup className="mt-3">
-                <FormControl as="textarea" id="description" name="description" placeholder="Description" rows={4} required />
-              </FormGroup>
-  
-              <FormGroup className="mt-3">
-                <div className="upload-container">
-                  <p>Add Cover image</p>
-                  <input type="file" name="cover-image" id="cover-image" />
-                </div>
-              </FormGroup>
-
-              <FormGroup className="mt-3">
-                <div className="upload-container">
-                  <p>Add product files</p>
-                  <input type="file" name="files" id="files" multiple/>
-                </div>
-              </FormGroup>
-  
-              <FormGroup className="mt-3">
-                <FormControl id="price" type="number" name="price" placeholder="Price" required style={{ width: '10%' }} />
-              </FormGroup>
-  
-              <FormGroup className="mt-3">
-                <Form.Select id="category" name="category" style={{ width: '25%' }}>
-                  <option value="Images">Category: Images</option>
-                  <option value="Videos">Category: Videos</option>
-                  <option value="Music">Category: Music</option>
-                  <option value="Services">Category: Services</option>
-                </Form.Select>
-              </FormGroup>
-  
-              <div className="d-flex justify-content-center">
-                <Button variant="secondary" type="button" className="mt-4 me-2" onClick={cancelHandler}>
-                  Cancel
-                </Button>
-                <Button variant="primary" type="submit" className="mt-4" style={{ backgroundColor: '#348B81' }}>
-                  Submit
-                </Button>
-              </div>
-            </Form>
-            </RouterForm>
-
-*/
