7994af8b58e92bc95c1ca9eb06f1c725590973f3
Nilay Setiya <79784352+nset793@users.noreply.github.com>
2023-05-08 23:21:53 +1200

    Added copy file functionality in seeding script

--- backend/db/seeding.js
+++ backend/db/seeding.js
@@ -1,16 +1,69 @@
-import mongoose from "mongoose";
 import { Account } from "../models/accountModel.js";
 import { Product } from "../models/productModel.js";
-import { ObjectId } from "mongoose";
 import fs from "fs";
+import path from "path";
+import util from "util";
+import { rimraf } from "rimraf";
 import { ProductReview } from "../models/productReviewModel.js";
-import { Message } from "../models/messageModel.js";
+
+const mkdir = util.promisify(fs.mkdir);
+const readdir = util.promisify(fs.readdir);
+const rmdir = util.promisify(fs.rmdir);
+const stat = util.promisify(fs.stat);
+const copyFile = util.promisify(fs.copyFile);
+const unlink = util.promisify(fs.unlink);
+const rimrafAsync = util.promisify(rimraf);
+
+function deleteFolder(folderPath) {
+  fs.readdir(folderPath, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      fs.unlink(path.join(folderPath, file), (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+}
+
+async function copyFolder(source, destination) {
+  try {
+    await mkdir(destination, { recursive: true });
+    const files = await readdir(source);
+
+    for (const file of files) {
+      const srcPath = path.join(source, file);
+      const destPath = path.join(destination, file);
+      const fileStat = await stat(srcPath);
+
+      if (fileStat.isDirectory()) {
+        await copyFolder(srcPath, destPath);
+      } else {
+        await copyFile(srcPath, destPath);
+      }
+    }
+
+    console.log(
+      `Folder '${source}' has been copied to '${destination}' successfully.`
+    );
+  } catch (error) {
+    console.error(`Error while copying folder: ${error.message}`);
+  }
+}
 
 async function importDataFromFile() {
   // Cleanup previous data
   await Account.deleteMany({});
   await Product.deleteMany({});
   await ProductReview.deleteMany({});
+
+  // Delete folders
+  deleteFolder("./public/uploads");
+  deleteFolder("./public/downloadFiles");
+
+  // Copy new folders
+  copyFolder("./db/uploads", "./public/uploads");
+  copyFolder("./db/downloadFiles", "./public/downloadFiles");
 
   // Read data from JSON file
   const data = JSON.parse(fs.readFileSync("./db/data.json", "utf-8"));
