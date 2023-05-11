687fa5a341e02b88971881465f813caa2b2aff64
jsan956-ai <jsan956@aucklanduni.ac.nz>
2023-05-12 01:02:52 +1200

    rating fix author page

--- frontend/src/components/AuthorLayout.jsx
+++ frontend/src/components/AuthorLayout.jsx
@@ -1,178 +1,224 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link, useNavigate, useSubmit } from "react-router-dom";
-import { InputGroup, DropdownButton, Dropdown } from 'react-bootstrap';
-import Card from '../pages/ui/Card';
-import { StarFill } from 'react-bootstrap-icons';
-import classes from '../pages/ui/Card.module.css'
-import './AuthorLayout.css'
+import { InputGroup, DropdownButton, Dropdown } from "react-bootstrap";
+import Card from "../pages/ui/Card";
+import { StarFill } from "react-bootstrap-icons";
+import classes from "../pages/ui/Card.module.css";
+import "./AuthorLayout.css";
 
-const AuthorLayout = ({ author, userAccountType, authorPageId, userId, isOwnAccount }) => { 
-    const submit = useSubmit();
-    const navigate = useNavigate();
-    // checking the account type of the author 
-    const isAdmin = (userAccountType === "admin");
+const AuthorLayout = ({
+  author,
+  userAccountType,
+  authorPageId,
+  userId,
+  isOwnAccount,
+}) => {
+  const submit = useSubmit();
+  const navigate = useNavigate();
+  // checking the account type of the author
+  const isAdmin = userAccountType === "admin";
 
-    console.log("++++++++++++++++++++++++++++++++++++++++++")
-    console.log(isAdmin, 14);
-    console.log("++++++++++++++++++++++++++++++++++++++++++")
+  console.log("++++++++++++++++++++++++++++++++++++++++++");
+  console.log(isAdmin, 14);
+  console.log("++++++++++++++++++++++++++++++++++++++++++");
 
-    // calculating assets sold and the average rating 
-    let averageRatingTemp = 0;
+  let averageRatingTemp = 0;
+  
+  author.sellingProducts.forEach((item) => {
+    const rating = parseFloat(item.averageRating.$numberDecimal);
+    averageRatingTemp += rating;
+});
 
-    author.sellingProducts.forEach((item) => {
-        averageRatingTemp += item.averageRating;
-    }); 
+const averageRating2 = (averageRatingTemp / author.sellingProducts.length).toFixed(1);
 
-    const averageRating = averageRatingTemp / author.sellingProducts.length;
+  const totalItems = author.sellingProducts.length;
+  const itemText = totalItems > 1 ? "assets" : "asset";
 
-    const totalItems = author.sellingProducts.length;
-    const itemText = totalItems > 1 ? 'assets' : 'asset';
+  const [a_title, setTitle] = useState("Sort by: Price: Low to High");
+  const handleSelect = (eventKey) => {
+    if (eventKey === "plth") {
+      setTitle("Sort by: Price: Low to High");
+    } else {
+      setTitle("Sort by: Price: High to Low");
+    }
+  };
 
-    const [a_title, setTitle] = useState('Sort by: Price: Low to High');
-    const handleSelect = (eventKey) => {
-        if (eventKey === 'plth') {
-          setTitle('Sort by: Price: Low to High');
-        } else {
-          setTitle('Sort by: Price: High to Low');
-        }
+  const deleteAccountHandler = () => {
+    const proceed = window.confirm("Are you sure?");
+
+    if (proceed) {
+      submit(null, { method: "DELETE" });
+    }
+  };
+
+  const createNewChatWithUser = async () => {
+    const chatData = {
+      account1: userId,
+      account2: authorPageId,
     };
 
-    const deleteAccountHandler = () => {
-        const proceed = window.confirm('Are you sure?');
+    // post endpoint to create a new chat
+    const response = await fetch("http://localhost:3000/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(chatData),
+    });
 
-        if (proceed){
-            submit(null, { method: 'DELETE' });
-        }
+    if (!response.ok) {
+      throw json(
+        {
+          message:
+            "Could not successfully create a new chat room with this user",
+        },
+        { status: 500 }
+      );
     }
 
-    const createNewChatWithUser = async () => {
+    const roomId = await response.json();
 
-        const chatData = {
-            account1: userId, 
-            account2: authorPageId
-        }
+    console.log(roomId, 70);
 
-        // post endpoint to create a new chat
-        const response = await fetch('http://localhost:3000/chat', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(chatData)
-        });
+    // navigate to the messages page
+    navigate("/store/profile/messages");
+  };
 
-        if (!response.ok){
-            throw json({ message: "Could not successfully create a new chat room with this user"}, { status: 500 });
-        }
-
-        const roomId = await response.json() 
-
-        console.log(roomId, 70);
-
-        // navigate to the messages page
-        navigate('/store/profile/messages');
-    }
-
-    return (
-        <div className='a_allcontainer'>
-            <div className='a_formargin'>
-                <div className='a_firstdiv'>
-                    <h1 className='a_authorname'>{author.username}</h1>
-                    <div className='a_forcontainbutton row'>
-                        {!isOwnAccount &&<div className='a_message col-sm-2'>
-                          <button className='a_message' onClick={createNewChatWithUser}>Message</button>
-                        </div>}
-                        {isAdmin && <div className='a_forcontainlink col-sm-10'>
-                          <button onClick={deleteAccountHandler}>Delete Account</button>
-                        </div>}
-                        {/* <button className='a_message'>Message</button> */}
-                    </div>
-                </div>
-                <div className='a_tablecontainer'>
-                    <table className="table table-borderless">
-                        <tbody>
-                            <tr>
-                                <td>Average Rating</td>
-                                <td>{averageRating}</td>
-                            </tr>
-                            {/* 另外一个页面删除 */}
-                            <tr>
-                                <td>Assets Sold</td>
-                                <td>{totalItems}</td>
-                            </tr>
-                            <tr>
-                                <td>Email</td>
-                                <td><p className='text-wrap'>{author.email}</p></td>
-                            </tr>
-                            {/* 结束删除 */}
-                        </tbody>
-                    </table>
-                </div>
-                <div className='a_cardpart d-flex justify-content-between'>
-                    <div>
-                        <h2>Showing {totalItems}&nbsp;{itemText} for sale</h2>
-                    </div>
-                    <div>
-                        <InputGroup>
-                            <DropdownButton
-                                variant="outline-secondary"
-                                title={a_title}
-                                id="sort-dropdown"
-                                align="end"
-                                onSelect={handleSelect}
-                                className='a_dropdownbutton'
-                            >
-                                <Dropdown.Item href="#/price-asc" eventKey="plth">Price: Low to High</Dropdown.Item>
-                                <Dropdown.Item href="#/price-desc" eventKey="phtl">Price: High to Low</Dropdown.Item>
-                            </DropdownButton>
-                        </InputGroup>
-                    </div>
-                </div>
-                <div className='a_cardpart2'>
-                    <ul className="row list-unstyled" >
-                        {author.sellingProducts.map((item) => (
-                        <li key={item._id} className="col-sm-4">
-                            <Card>
-                                <div className={`${classes.authorpagecard}`}>
-                                    <div className={`${classes.authorimgcontainer}`}>
-                                        <img src={'http://localhost:3000/uploads/' + item.coverImage}/>
-                                    </div>
-                                    <div className={`${classes.contentcontainer}`}>
-                                        <div className="row">
-                                            <div className={`${classes.productcontainer} col-md-8`}>
-                                                <Link id="productLink" to={`/store/product/${item._id}`}>
-                                                    <p className="text-nowrap text-truncate">{item.name}</p>
-                                                </Link>
-                                            </div>
-                                            <div className={`${classes.price} col-md-4`} >
-                                                <p>${Math.floor(item.price)}
-                                                    <span>{(item.price % 1).toFixed(2).split('.')[1]}</span>
-                                                </p>
-                                            </div>
-                                        </div>
-
-                                        <div className="d-flex justify-content-between">
-                                            <div>
-                                                {/* <p className="text-nowrap text-truncate">{item.description}</p> */}
-                                            </div>
-                                            <div>
-                                                <h5>{item.category}</h5>
-                                            </div>
-                                        </div>
-                                        <div className="d-flex justify-content-between flex-wrap">
-                                            <h5>{item.amountSold} Sold</h5>
-                                            <h5 className="float-right"> <StarFill color="black" size={18} />&nbsp;{item.averageRating.toFixed(1)}</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                            </Card> 
-                        </li>
-                    ))}
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className="a_allcontainer">
+      <div className="a_formargin">
+        <div className="a_firstdiv">
+          <h1 className="a_authorname">{author.username}</h1>
+          <div className="a_forcontainbutton row">
+            {!isOwnAccount && (
+              <div className="a_message col-sm-2">
+                <button className="a_message" onClick={createNewChatWithUser}>
+                  Message
+                </button>
+              </div>
+            )}
+            {isAdmin && (
+              <div className="a_forcontainlink col-sm-10">
+                <button onClick={deleteAccountHandler}>Delete Account</button>
+              </div>
+            )}
+            {/* <button className='a_message'>Message</button> */}
+          </div>
         </div>
-    );
-}
+        <div className="a_tablecontainer">
+          <table className="table table-borderless">
+            <tbody>
+              <tr>
+                <td>Average Rating</td>
+                <td>{averageRating2}</td>
+              </tr>
+              {/* 另外一个页面删除 */}
+              <tr>
+                <td>Assets Sold</td>
+                <td>{totalItems}</td>
+              </tr>
+              <tr>
+                <td>Email</td>
+                <td>
+                  <p className="text-wrap">{author.email}</p>
+                </td>
+              </tr>
+              {/* 结束删除 */}
+            </tbody>
+          </table>
+        </div>
+        <div className="a_cardpart d-flex justify-content-between">
+          <div>
+            <h2>
+              Showing {totalItems}&nbsp;{itemText} for sale
+            </h2>
+          </div>
+          <div>
+            <InputGroup>
+              <DropdownButton
+                variant="outline-secondary"
+                title={a_title}
+                id="sort-dropdown"
+                align="end"
+                onSelect={handleSelect}
+                className="a_dropdownbutton"
+              >
+                <Dropdown.Item href="#/price-asc" eventKey="plth">
+                  Price: Low to High
+                </Dropdown.Item>
+                <Dropdown.Item href="#/price-desc" eventKey="phtl">
+                  Price: High to Low
+                </Dropdown.Item>
+              </DropdownButton>
+            </InputGroup>
+          </div>
+        </div>
+        <div className="a_cardpart2">
+          <ul className="row list-unstyled">
+            {author.sellingProducts.map((item) => (
+              <li key={item._id} className="col-sm-4">
+                <Card>
+                  <div className={`${classes.authorpagecard}`}>
+                    <div className={`${classes.authorimgcontainer}`}>
+                      <img
+                        src={"http://localhost:3000/uploads/" + item.coverImage}
+                      />
+                    </div>
+                    <div className={`${classes.contentcontainer}`}>
+                      <div className="row">
+                        <div className={`${classes.productcontainer} col-md-8`}>
+                          <Link
+                            id="productLink"
+                            to={`/store/product/${item._id}`}
+                          >
+                            <p className="text-nowrap text-truncate">
+                              {item.name}
+                            </p>
+                          </Link>
+                        </div>
+                        <div className={`${classes.price} col-md-4`}>
+                          <p>
+                            ${Math.floor(item.price)}
+                            <span>
+                              {(item.price % 1).toFixed(2).split(".")[1]}
+                            </span>
+                          </p>
+                        </div>
+                      </div>
+
+                      <div className="d-flex justify-content-between">
+                        <div>
+                          {/* <p className="text-nowrap text-truncate">{item.description}</p> */}
+                        </div>
+                        <div>
+                          <h5>{item.category}</h5>
+                        </div>
+                      </div>
+                      <div className="d-flex justify-content-between flex-wrap">
+                        <h5>{item.amountSold} Sold</h5>
+                        <h5 className="float-right">
+                          <StarFill color="black" size={18} />
+                          &nbsp;
+                          {parseFloat(
+                            item.averageRating.$numberDecimal
+                          ).toFixed(1)}
+                        </h5>
+                      </div>
+                    </div>
+                  </div>
+                </Card>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AuthorLayout;
+
+/*
+
+    {item.reviews.length > 0 ? item.averageRating.toFixed(1) : "Not available"}
+*/
