1d18c7969df62c732974c8e995d2ed338597cdaf
LGoGoGo <19931299891@163.com>
2023-05-12 21:04:36 +1200

    Update StoreRootPage.jsx

--- frontend/src/pages/StoreRootPage.jsx
+++ frontend/src/pages/StoreRootPage.jsx
@@ -112,9 +112,6 @@ export const action = async ({params, request}) => {
   // reload the page so the page gets updated
   window.location.reload();
 
-  // reload the page so the page gets updated
-  window.location.reload();
-
   return redirect('/store/product/' + productId);
 };
   
